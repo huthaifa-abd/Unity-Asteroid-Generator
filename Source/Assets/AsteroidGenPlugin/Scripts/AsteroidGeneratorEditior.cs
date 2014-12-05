e98b76e1070ec657867f88a85b1818a0dafa8fe3
huthaifa <huth2004@gmail.com>
2014-12-05 23:31:59 +0300

    General Updates And Fixes

--- Source/Assets/AsteroidGenPlugin/Scripts/AsteroidGeneratorEditior.cs
+++ Source/Assets/AsteroidGenPlugin/Scripts/AsteroidGeneratorEditior.cs
@@ -5,6 +5,8 @@ using UnityEditor;
 [CustomEditor(typeof(Asteroid_Generator))]
 class AsteroidGeneratorEditior : Editor
 {
+    public GameObject objGizmo;
+    public bool _issub = false;
     public override void OnInspectorGUI()
     {
 
@@ -34,8 +36,28 @@ class AsteroidGeneratorEditior : Editor
             ScriptHandler.funcClearAsteroid();
         }
 
+        GUILayout.Label("Gizmo Configuration");
+        GUILayout.Label("Used to add or subtract asteroid regions");
 
         
+       objGizmo = (GameObject)EditorGUILayout.ObjectField("Gizmo Mesh", objGizmo, typeof(GameObject), true);
+       _issub = EditorGUILayout.Toggle("Subtract Region", _issub);
+
+        if (GUILayout.Button("Apply Gizmo"))
+        {
+            Asteroid_Generator ScriptHandler = (Asteroid_Generator)target;
+            //Apply Gizmo Collision
+            if (objGizmo != null)
+            {
+                //Set Gizmo Layermask
+                objGizmo.layer = 8;
+                ScriptHandler.funcApplyGizmo(objGizmo.name, 1000f, objGizmo.collider, _issub);
+            }
+            else
+                Debug.LogError("Gizmo Object is null , make sure its avaliable");
+            
+        }
+       
         GUILayout.Label("Asteroid Generation Plugin v0.1");
 
         
--- Source/Assets/AsteroidGenPlugin/Scripts/Asteroid_Generator.cs
+++ Source/Assets/AsteroidGenPlugin/Scripts/Asteroid_Generator.cs
@@ -18,7 +18,7 @@ public class Asteroid_Generator : MonoBehaviour
     public float MinAsteroidSize = 0.1f;
     public bool SetAnimation = false;
     public int AnimationSpeed = 1;
-    
+
 
     void Update()
     {
@@ -32,6 +32,99 @@ public class Asteroid_Generator : MonoBehaviour
             DestroyImmediate(child.gameObject);
         }
 
+    }
+
+    public void funcApplyGizmo(string GizmoName, float CastDistance, Collider GizmoCollider, bool _issub)
+    {
+        float AffectedObjectCount = 0;
+        var tempList = transform.Cast<Transform>().ToList();
+        foreach (var child in tempList)
+        {
+
+            if (_issub)
+            {
+                if (RaycastTest(child.gameObject, CastDistance, GizmoName))
+                {
+                    DestroyImmediate(child.gameObject);
+                    AffectedObjectCount++;
+                }
+            }
+            else
+            {
+                if (!RaycastTest(child.gameObject, CastDistance, GizmoName))
+                {
+                    DestroyImmediate(child.gameObject);
+                    AffectedObjectCount++;
+                }
+            }
+
+        }
+
+        Debug.Log("Affected Object Count : " + AffectedObjectCount.ToString());
+    }
+
+    public bool RaycastTest(GameObject Object, float CastDistance, string TargetName)
+    {
+        //RaycastHit hit;
+        //bool is_hit = false;
+        bool _inside = false;
+
+        //Vector3 forward = Object.transform.TransformDirection(Vector3.forward);
+        //if (Physics.Raycast(Object.transform.position, forward, out hit, CastDistance))
+        //    if (hit.transform.name == TargetName)
+        //    {
+        //        is_hit = true;
+        //    }
+
+
+        //Vector3 backward = Object.transform.TransformDirection(Vector3.back);
+        //if (Physics.Raycast(Object.transform.position, backward, out hit, CastDistance))
+        //    if (hit.transform.name == TargetName)
+        //    {
+        //        is_hit = true;
+        //    }
+
+
+        //Vector3 upward = Object.transform.TransformDirection(Vector3.up);
+        //if (Physics.Raycast(Object.transform.position, upward, out hit, CastDistance))
+        //    if (hit.transform.name == TargetName)
+        //    {
+        //        is_hit = true;
+        //    }
+
+
+
+        //Vector3 downward = Object.transform.TransformDirection(Vector3.down);
+        //if (Physics.Raycast(Object.transform.position, downward, out hit, CastDistance))
+        //    if (hit.transform.name == TargetName)
+        //    {
+        //        is_hit = true;
+        //    }
+
+
+        Vector3[] raycastDirections = new Vector3[5];
+        raycastDirections[0] = new Vector3(0, 1, 0);
+        raycastDirections[1] = new Vector3(0, -1, -0);
+        raycastDirections[2] = new Vector3(0, 0, 1);
+        raycastDirections[3] = new Vector3(-1.41f, 0, -0.5f);
+        raycastDirections[4] = new Vector3(1.41f, 0, -0.5f);
+        int hitcount = 0;
+
+        foreach (Vector3 direction in raycastDirections)
+        {
+            Debug.DrawRay(Object.transform.position - CastDistance * direction, direction * CastDistance, Color.green);
+            if (Physics.Raycast(Object.transform.position - CastDistance * direction, direction))
+            {
+                hitcount++;
+            }
+        }
+
+        if (hitcount >= 5)
+            _inside = true;
+        else
+            _inside = false;
+
+        return _inside;
     }
     public void funcGenerateAsteroid()
     {
@@ -165,6 +258,8 @@ public class Asteroid_Generator : MonoBehaviour
         }
         return YMovement;
     }
+
+
 
 
 }
